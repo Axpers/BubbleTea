3c5a4265d5f8dbe5a1752e58dd1d68163ae9ea1c
Garik Derminjyan <20193733@etud.univ-evry.fr>
2021-04-24 19:02:40 +0200

    ajout de quelques propriétés css pour vérifier que tout fonctionne

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const App = function () {
   return (
-    <div id="root" className="mainContainer">
+    <div id="root" className="main-container">
       <div id="generator">Bubble Tea Generator</div>
       <div id="core">Core part</div>
     </div>
