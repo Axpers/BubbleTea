3dd869a7f1b40687afee0662e55a9f7bc5c90688
Garik Derminjyan <20193733@etud.univ-evry.fr>
2021-04-28 11:23:22 +0200

    Ajout des élements dans la sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Sidebar = function () {
-  return <div id="sidebar">Bubble Tea Generator</div>;
+  return (
+    <div id="sidebar">
+      <h1 id="titre-generator">Bubble Tea Generator</h1>
+      <div id="ingredient-generator">
+        <h2>Nom du Bubble Tea</h2>
+        <h2>Gobelet</h2>
+        <h2>Tea</h2>
+        <h2>Tapioca</h2>
+        <h2>Taille</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
