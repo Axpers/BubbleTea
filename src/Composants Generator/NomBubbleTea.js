662291773002d697d0b83fedcbf8dc5035ca2b31
Garik Derminjyan <20193733@etud.univ-evry.fr>
2021-04-28 19:57:22 +0200

    Ajout de l'utilisation de useState pour le nom dynamique

--- src/Composants Generator/NomBubbleTea.js
+++ src/Composants Generator/NomBubbleTea.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NomBubbleTea = function () {
+  const [nameBT, setNameBT] = useState("Test");
   return (
     <div className="conteneurLabelInput">
-      <label htmlFor="nameBubbleTea">Nom BubbleTea</label>
-      <input type="text" name="nameBubbleTea" />
+      <label htmlFor="nameBubbleTea">NOM</label>
+      <input
+        type="text"
+        name="nameBubbleTea"
+        id="nameBubbleTea"
+        value={nameBT}
+        onChange={(e) => setNameBT(e.target.value)}
+      />
     </div>
   );
 };
