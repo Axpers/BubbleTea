d6efba8d00285afaf8956ab078ac62ba5b7da2fb
Garik Derminjyan <20193733@etud.univ-evry.fr>
2021-04-30 15:39:41 +0200

    Mise en place de la variable qui gère le scale du Svg

--- src/Ingredients/RenduGobelet.js
+++ src/Ingredients/RenduGobelet.js
@@ -6,10 +6,14 @@ import NameBTContext from "../ContextVariables/NameBTContext";
 
 const RenduGobelet = function () {
   const nameBT = useContext(NameBTContext);
+  const test = "0.6";
+  const svgStyle = {
+    transform: `scale(${test})`,
+  };
 
   return (
     <div id="rendu">
-      <svg id="svg-rendu">
+      <svg id="svg-rendu" style={svgStyle}>
         <Gobelet />
         <Tea />
         <Tapioca />
