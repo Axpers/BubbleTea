dab15b9910f5ab6d2827916899529fd8f95dad93
Garik Derminjyan <20193733@etud.univ-evry.fr>
2021-04-30 21:35:57 +0200

    Mise en place d'une fonction a appeler à chaque changement d'option de select

--- src/ComposantsGenerator/SetCouleurs.js
+++ src/ComposantsGenerator/SetCouleurs.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 const SetCouleurs = function () {
+  let val1 = 0;
+  console.log(val1);
+
+  const test = function () {
+    val1++;
+    console.log(val1);
+  };
+
   return (
     <div>
       <label htmlFor="setCouleur">Set prédéfini</label>
-      <select name="setCouleur" id="setCouleur">
+      <select name="setCouleur" id="setCouleur" onChange={test}>
         <option value="">Choix de Set</option>
-        <option value="">Y'a pas de set dsl</option>
+        <option value="">Pas de set</option>
       </select>
     </div>
   );
