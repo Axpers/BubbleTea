b5f350cc14801f970fa9a64bae5ba224a140d2d0
Garik Derminjyan <20193733@etud.univ-evry.fr>
2021-05-01 11:28:57 +0200

    Modification du composant SetCouleurs et la fonction qui gère le changement à chaque clic

--- src/ComposantsGenerator/SetCouleurs.js
+++ src/ComposantsGenerator/SetCouleurs.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import TapiocaColorContext from "../ContextVariables/TapiocaColorContext";
+import TeaColorContext from "../ContextVariables/TeaColorContext";
 
 const SetCouleurs = function () {
-  let val1 = 0;
-  console.log(val1);
+  let valeurSet = useState("Thème par défaut");
+  let couleurTapioca = useContext(TapiocaColorContext);
+  let couleurTea = useContext(TeaColorContext);
 
-  const test = function () {
-    val1++;
-    console.log(val1);
-  };
+  function changementCouleurs(element) {
+    if (element === "clair") {
+      valeurSet = element;
+      couleurTapioca = "#FFFFFF";
+      couleurTea = "#FFFFFF";
+    } else if (element === "sombre") {
+      valeurSet = element;
+      couleurTapioca = "#000000";
+      couleurTea = "#000000";
+    }
+  }
 
   return (
     <div>
-      <label htmlFor="setCouleur">Set prédéfini</label>
-      <select name="setCouleur" id="setCouleur" onChange={test}>
-        <option value="">Choix de Set</option>
-        <option value="">Pas de set</option>
+      <label htmlFor="setCouleur">Set Couleurs</label>
+      <select
+        multiple={true}
+        name="setCouleur"
+        id="setCouleur"
+        value={valeurSet}
+        onChange={(element) => changementCouleurs(element.target.value)}
+      >
+        <option value="clair">Thème Clair</option>
+        <option value="sombre">Thème Sombre</option>
       </select>
     </div>
   );
